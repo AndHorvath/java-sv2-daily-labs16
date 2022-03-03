290a153deafabfb0b3625f766e8416c5406925d4
András Horváth <horvath.andras@prevaudit.hu>
2022-03-03 22:28:53 +0100

    day02 done

---
+++ week16-lab-tasks/day02/Daily_Tasks_CREATE.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE suli;
+
+CREATE TABLE jegy (
+	id INT NOT NULL AUTO_INCREMENT,
+	ertek INT,
+	leiras VARCHAR(10),
+	datum DATE,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE diak (
+	id INT NOT NULL AUTO_INCREMENT,
+	nev VARCHAR (30),
+	szuldatum DATE,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE naplo (
+	id INT NOT NULL AUTO_INCREMENT,
+	leiras VARCHAR(10),
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE jegy
+ADD COLUMN diak_id INT;
